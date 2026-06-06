550a31e29f414d60fdb92d70a94ffa839e5a1df6
edyribowo <eribowo9@gmail.com>
2026-06-06 22:28:05 +0700

    xpath shipping type

--- src/test/java/com/indomaret/klikindomaret/identifier/CartPageIdentifier.java
+++ src/test/java/com/indomaret/klikindomaret/identifier/CartPageIdentifier.java
@@ -2,14 +2,14 @@ package com.indomaret.klikindomaret.identifier;
 
 public class CartPageIdentifier {
     public static final String CART_TITLE_HEADER = "//android.widget.TextView[@text=\"Keranjang Belanja\"]";
-    public static final String SHIPPING_TYPE_MENU = "com.indomaret.klikindomaret:id/bq6";
+    public static final String SHIPPING_TYPE_MENU = "com.indomaret.klikindomaret:id/43p";
     public static final String CONFIRM_SLOT_BUTTON = "com.indomaret.klikindomaret:id/did";
     public static final String CHECK_PROMO_OR_COUPON = "com.indomaret.klikindomaret:id/ffq";
     public static final String BELANJA_XTRA_MENU = "com.indomaret.klikindomaret:id/aqk";
     public static final String BANNER_SHIPPING_XTRA = "//android.widget.FrameLayout[@resource-id=\"com.indomaret.klikindomaret:id/33\"]" +
             "/androidx.cardview.widget.CardView/android.view.ViewGroup";
     public static final String DETAIL_PAYMENT_EXPAND = "com.indomaret.klikindomaret:id/867";
-    public static final String PRODUCT_PRICE = "com.indomaret.klikindomaret:id/867";
+    public static final String PRODUCT_PRICE = "com.indomaret.klikindomaret:id/ed0";
     public static final String SHIPPING_FEE = "com.indomaret.klikindomaret:id/gfh";
     public static final String INSURANCE_FEE = "com.indomaret.klikindomaret:id/xpathinsurancefee";
     public static final String TOTAL_PAYMENT = "com.indomaret.klikindomaret:id/297"; // Note: update this ID if needed
